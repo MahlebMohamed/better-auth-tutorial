71714b1b5de9adb2a946d9354904360c2022509f
MahlebMohamed <mohamed_mahleb@hotmail.com>
2025-12-25 14:07:49 +0100

    feat: add various UI components and authentication features
    - Introduced LoadingButton component for loading states in buttons.
    - Added ModeToggle component for theme switching.
    - Implemented PasswordInput component with show/hide password functionality.
    - Created Avatar, Badge, Button, Card, Checkbox, DropdownMenu, Form, Input, Label, Skeleton, and Toaster components for UI consistency.
    - Developed UserAvatar and UserDropdown components for user profile management.
    - Integrated authentication with better-auth, including sign-in, sign-up, and email verification functionalities.
    - Established email sending capabilities using Resend.
    - Added validation schemas for user sign-up and sign-in processes.
    - Configured TypeScript settings for improved development experience.

---
+++ src/lib/auth.ts
@@ -0,0 +1,35 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from "@/lib/prisma";
+import { sendEmail } from "./email";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
+    async sendVerificationEmail({ user, url }) {
+      await sendEmail({
+        to: user.email!,
+        subject: "Verify your email",
+        text: `Click the following link to verify your email: ${url}`,
+      });
+    },
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        input: false,
+      },
+    },
+  },
+});
+
+export type Session = typeof auth.$Infer.Session;
+export type User = typeof auth.$Infer.Session.user;
---
+++ src/lib/email.ts
@@ -0,0 +1,18 @@
+import { Resend } from "resend";
+
+interface sendEmailProps {
+  to: string;
+  subject: string;
+  text: string;
+}
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function sendEmail({ to, subject, text }: sendEmailProps) {
+  await resend.emails.send({
+    from: "mohamed_mahleb@hotmail.com",
+    to,
+    subject,
+    text,
+  });
+}
